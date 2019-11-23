b76279a94aa4ac98e22d030fb3d028b95e8cefd1
Paul Jickling <pjickling@gmail.com>
2019-11-23 10:04:43 -0800

    Added sample images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Image from './Image';
+import Sample from './sample.jpg';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello world</p>
+      <p>Standard Image</p>
+      <img src={Sample} alt="Unaltered landscape" />
+      <p>4:3 Image</p>
+      <Image aspectRatio="43" link={Sample} text="4:3 landscape" />
+      <p>16:9 Image</p>
+      <Image aspectRatio="169" link={Sample} text="16:9 landscape" />
     </div>
   );
 }
