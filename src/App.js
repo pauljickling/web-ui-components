c3b089770a39032dcfcdc8046d73be85c38f2580
Paul Jickling <pjickling@gmail.com>
2019-11-27 10:00:32 -0800

    Fixed some issues with Image component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Sample from './sample.jpg';
 function App() {
   return (
     <div className="App">
-      <p>Standard Image</p>
-      <img src={Sample} alt="Unaltered landscape" />
-      <p>4:3 Image</p>
-      <Image aspectRatio="43" link={Sample} text="4:3 landscape" />
-      <p>16:9 Image</p>
-      <Image aspectRatio="169" link={Sample} text="16:9 landscape" />
+      <div className="container">
+        <p>Standard Image</p>
+        <img src={Sample} alt="Unaltered landscape" />
+        <p>4:3 Image</p>
+        <Image aspectRatio="43" link={Sample} text="4:3 landscape" />
+        <p>16:9 Image</p>
+        <Image aspectRatio="169" link={Sample} text="16:9 landscape" />
+      </div>
     </div>
   );
 }
