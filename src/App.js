088682208374aacdabd9b12b66d3e2fa212faffa
Paul Jickling <pjickling@gmail.com>
2019-12-01 13:47:40 -0800

    Experimented with List component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Image from './Image';
 import Sample from './sample.jpg';
+import List from './List';
+import ListItem from './ListItem';
 
-function App() {
+function listCount(item) {
+  let count = item.props.children.length;
+	return <p>{count}</p>;
+}
+
+function App(props) {
   return (
     <div className="App">
       <div className="container">
+        {listCount(
+        <List> 
+          <ListItem text="1" />
+          <ListItem text="2" />
+          <ListItem text="3" />
+        </List>)}
+
+        <List> 
+          <ListItem text="1" />
+          <ListItem text="2" />
+          <ListItem text="3" />
+          <ListItem text="4" />
+        </List>
+
         <p>Standard Image</p>
         <img src={Sample} alt="Unaltered landscape" />
         <p>4:3 Image</p>
